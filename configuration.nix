1bc19394429fdf1138eeaaad26499f22784cb8da
Bruno St John <brunost.john@icloud.com>
2025-05-04 16:24:55 +0100

    nixos wsi

--- configuration.nix
+++ configuration.nix
@@ -168,7 +168,53 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [ git wget lm_sensors mangohud ];
+  environment.systemPackages = with pkgs; [
+    git
+    wget
+    lm_sensors
+    mangohud
+    (stdenv.mkDerivation rec {
+      pname = "vulkan-hdr-layer";
+      version = "63d2eec";
+
+      src = (fetchFromGitHub {
+        owner = "Zamundaaa";
+        repo = "VK_hdr_layer";
+        rev = "869199cd2746e7f69cf19955153080842b6dacfc";
+        fetchSubmodules = true;
+        hash = "sha256-xfVYI+Aajmnf3BTaY2Ysg5fyDO6SwDFGyU0L+F+E3is=";
+      }).overrideAttrs (_: {
+        GIT_CONFIG_COUNT = 1;
+        GIT_CONFIG_KEY_0 = "url.https://github.com/.insteadOf";
+        GIT_CONFIG_VALUE_0 = "git@github.com:";
+      });
+
+      nativeBuildInputs = [ vulkan-headers meson ninja pkg-config jq ];
+
+      buildInputs = [
+        vulkan-headers
+        vulkan-loader
+        vulkan-utility-libraries
+        xorg.libX11
+        xorg.libXrandr
+        wayland-scanner
+        xorg.libxcb
+        wayland
+      ];
+
+      # Help vulkan-loader find the validation layers
+      setupHook = writeText "setup-hook" ''
+        addToSearchPath XDG_DATA_DIRS @out@/share
+      '';
+
+      meta = with lib; {
+        description = "Layers providing Vulkan HDR";
+        homepage = "https://github.com/Zamundaaa/VK_hdr_layer";
+        platforms = platforms.linux;
+        license = licenses.mit;
+      };
+    })
+  ];
 
   networking.firewall.enable = false;
   system.stateVersion = "24.11";
